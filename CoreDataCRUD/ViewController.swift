29ef0b4b3fff38c193940e40ba10a4fbab8e730f
tomi <tomisin18@gmail.com>
2019-06-19 14:50:00 +0100

    Initial Commit

---
+++ CoreDataCRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by OYEGOKE TOMISIN on 19/06/2019.
+//  Copyright © 2019 OYEGOKE TOMISIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
